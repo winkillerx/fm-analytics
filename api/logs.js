f3febea55ccbbd8f6a115c3e073d481d32763cb5
winkillerx <the_hump_god@hotmail.com>
2026-02-21 23:10:18 -0400

    Update logs.js

--- api/logs.js
+++ api/logs.js
@@ -1,6 +1,23 @@
 // api/logs.js
 
+const ALLOWED_ORIGINS = [
+  "https://filmmatrix.net",
+  "https://www.filmmatrix.net"
+];
+
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/json");
+  const origin = req.headers.origin;
+
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   res.status(200).json(globalThis.FM_LOGS || []);
 }
